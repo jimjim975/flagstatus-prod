7bd2c8cfacf69dbb87ed7ebbae47a96c969de316
jimjim975 <jim@rjgdesign.com>
2024-03-28 14:47:35 -0400

    fixing my fuck up

--- src/fuckingshit.js
+++ src/fuckingshit.js
@@ -54,6 +54,33 @@ const formatRows = (rows) => {
 }
 
 const regions = [{name: "United States", abbreviation:"US"},{name:"Alabama",abbreviation:"AL"},{name:"Alaska",abbreviation:"AK"},{name:"Arizona",abbreviation:"AZ"},{name:"Arkansas",abbreviation:"AR"},{name:"California",abbreviation:"CA"},{name:"Colorado",abbreviation:"CO"},{name:"Connecticut",abbreviation:"CT"},{name:"Delaware",abbreviation:"DE"},{name:"Florida",abbreviation:"FL"},{name:"Georgia",abbreviation:"GA"},{name:"Hawaii",abbreviation:"HI"},{name:"Idaho",abbreviation:"ID"},{name:"Illinois",abbreviation:"IL"},{name:"Indiana",abbreviation:"IN"},{name:"Iowa",abbreviation:"IA"},{name:"Kansas",abbreviation:"KS"},{name:"Kentucky",abbreviation:"KY"},{name:"Louisiana",abbreviation:"LA"},{name:"Maine",abbreviation:"ME"},{name:"Maryland",abbreviation:"MD"},{name:"Massachusetts",abbreviation:"MA"},{name:"Michigan",abbreviation:"MI"},{name:"Minnesota",abbreviation:"MN"},{name:"Mississippi",abbreviation:"MS"},{name:"Missouri",abbreviation:"MO"},{name:"Montana",abbreviation:"MT"},{name:"Nebraska",abbreviation:"NE"},{name:"Nevada",abbreviation:"NV"},{name:"New Hampshire",abbreviation:"NH"},{name:"New Jersey",abbreviation:"NJ"},{name:"New Mexico",abbreviation:"NM"},{name:"New York",abbreviation:"NY"},{name:"North Carolina",abbreviation:"NC"},{name:"North Dakota",abbreviation:"ND"},{name:"Ohio",abbreviation:"OH"},{name:"Oklahoma",abbreviation:"OK"},{name:"Oregon",abbreviation:"OR"},{name:"Pennsylvania",abbreviation:"PA"},{name:"Rhode Island",abbreviation:"RI"},{name:"South Carolina",abbreviation:"SC"},{name:"South Dakota",abbreviation:"SD"},{name:"Tennessee",abbreviation:"TN"},{name:"Texas",abbreviation:"TX"},{name:"Utah",abbreviation:"UT"},{name:"Vermont",abbreviation:"VT"},{name:"Virginia",abbreviation:"VA"},{name:"Washington",abbreviation:"WA"},{name:"West Virginia",abbreviation:"WV"},{name:"Wisconsin",abbreviation:"WI"},{name:"Wyoming",abbreviation:"WY"}]
+
+function Idk(){
+  const [searchResult, setSearchResult] = useState();
+  async function getAPI(domain) {
+    try {
+      console.log("Trying to connect to: " + domain);
+      const response = await axios({
+        method: 'get',
+        url: domain,
+        withCredentials: false,
+      });
+      console.log(response);
+      setSearchResult(response.data);
+  
+    } catch (error) {
+      setSearchResult(null);
+    }
+  }
+
+  var domain = "https://api.flagstatus.org/status";
+  getAPI(domain);
+  return (
+    <div>{searchResult && searchResult.Status.map((searchResult, index) => {
+      return <li key={index}><span class="tab"></span>Date: {searchResult[0]}, Time: {searchResult[1]},Temperature: {searchResult[2]}</li>})}</div>
+  )
+}
+
 const App = () => {
   const [selector, setSelector] = useState("");
   const [rows, setRows] = useState([]);
@@ -79,6 +106,7 @@ const App = () => {
   };
   return (
     <div className="App">
+      <Idk></Idk>
       <Box sx={{ margin: 5}}>
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Region</InputLabel>
--- src/thing.py
+++
@@ -1,7 +0,0 @@
-f = open("file.txt", "r")
-newString = []
-for items in f:
-    newString.append("Backup job " + items[:-1] + " finished with Success.")
-
-for i in newString:
-    print(i)
