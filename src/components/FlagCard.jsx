8c69ae83ff22f437954a2ea59ed2835cd5ba1902
Jason D <jason.alex.d@protonmail.com>
2024-03-31 21:50:25 -0400

    made event display more minimal

--- src/components/FlagCard.jsx
+++ src/components/FlagCard.jsx
@@ -1,5 +1,5 @@
 import IconButton from '@mui/material/IconButton';
-import { Paper, Grid, Box, Typography } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import * as React from 'react';
 
@@ -10,20 +10,18 @@ function FlagCard({ description, startDate, endDate, state }) {
 
     return (
         <Box width="100%">
-            <Paper sx={{ my: 1, padding: 2, backgroundColor: "#282828" }} elevation={20}>
-                <Grid container justifyContent="space-between">
-                    <Grid>
-                        <Typography sx={{ fontSize: "2rem", color: "#B6B6B6", textAlign: "left" }}>{state}</Typography>
-                    </Grid>
-                    <Grid>
-                        <IconButton onClick={() => handleCopy(description)}><ContentCopyIcon sx={{ color: "#378CE7" }} /></IconButton>
-                    </Grid>
+            <Grid container justifyContent="space-between">
+                <Grid>
+                    <Typography sx={{ fontSize: "2rem", color: "#B6B6B6", textAlign: "left" }}>{state}</Typography>
                 </Grid>
-                <Typography sx={{ fontSize: "1.2rem", color: "#B6B6B6", textAlign: "left" }}>{startDate} - {endDate}</Typography>
-                <Box sx={{ marginTop: 1 }}>
-                    <Typography sx={{ fontSize: "0.9rem", color: "#B6B6B6", textAlign: "left" }}>{description}</Typography>
-                </Box>
-            </Paper>
+                <Grid>
+                    <IconButton onClick={() => handleCopy(description)}><ContentCopyIcon sx={{ color: "#378CE7" }} /></IconButton>
+                </Grid>
+            </Grid>
+            <Typography sx={{ fontSize: "1.2rem", color: "#B6B6B6", textAlign: "left" }}>{startDate} - {endDate}</Typography>
+            <Box sx={{ marginTop: 1 }}>
+                <Typography sx={{ fontSize: "0.9rem", color: "#B6B6B6", textAlign: "left" }}>{description}</Typography>
+            </Box>
         </Box>
     )
 }
--- src/features/FlagEvents/index.jsx
+++ src/features/FlagEvents/index.jsx
@@ -6,6 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import FlagCard from '../../components/FlagCard';
 import getFlagStatus from '../../services/getFlagStatus'
+import { Divider } from '@mui/material';
 
 const regions = [{name: "United States", abbreviation:"US"},{name:"Alabama",abbreviation:"AL"},{name:"Alaska",abbreviation:"AK"},{name:"Arizona",abbreviation:"AZ"},{name:"Arkansas",abbreviation:"AR"},{name:"California",abbreviation:"CA"},{name:"Colorado",abbreviation:"CO"},{name:"Connecticut",abbreviation:"CT"},{name:"Delaware",abbreviation:"DE"},{name:"Florida",abbreviation:"FL"},{name:"Georgia",abbreviation:"GA"},{name:"Hawaii",abbreviation:"HI"},{name:"Idaho",abbreviation:"ID"},{name:"Illinois",abbreviation:"IL"},{name:"Indiana",abbreviation:"IN"},{name:"Iowa",abbreviation:"IA"},{name:"Kansas",abbreviation:"KS"},{name:"Kentucky",abbreviation:"KY"},{name:"Louisiana",abbreviation:"LA"},{name:"Maine",abbreviation:"ME"},{name:"Maryland",abbreviation:"MD"},{name:"Massachusetts",abbreviation:"MA"},{name:"Michigan",abbreviation:"MI"},{name:"Minnesota",abbreviation:"MN"},{name:"Mississippi",abbreviation:"MS"},{name:"Missouri",abbreviation:"MO"},{name:"Montana",abbreviation:"MT"},{name:"Nebraska",abbreviation:"NE"},{name:"Nevada",abbreviation:"NV"},{name:"New Hampshire",abbreviation:"NH"},{name:"New Jersey",abbreviation:"NJ"},{name:"New Mexico",abbreviation:"NM"},{name:"New York",abbreviation:"NY"},{name:"North Carolina",abbreviation:"NC"},{name:"North Dakota",abbreviation:"ND"},{name:"Ohio",abbreviation:"OH"},{name:"Oklahoma",abbreviation:"OK"},{name:"Oregon",abbreviation:"OR"},{name:"Pennsylvania",abbreviation:"PA"},{name:"Rhode Island",abbreviation:"RI"},{name:"South Carolina",abbreviation:"SC"},{name:"South Dakota",abbreviation:"SD"},{name:"Tennessee",abbreviation:"TN"},{name:"Texas",abbreviation:"TX"},{name:"Utah",abbreviation:"UT"},{name:"Vermont",abbreviation:"VT"},{name:"Virginia",abbreviation:"VA"},{name:"Washington",abbreviation:"WA"},{name:"West Virginia",abbreviation:"WV"},{name:"Wisconsin",abbreviation:"WI"},{name:"Wyoming",abbreviation:"WY"}]
 
@@ -29,7 +30,7 @@ function FlagEvents() {
 
   return (
     <Box>
-      <FormControl fullWidth>
+      <FormControl fullWidth sx={{ marginBottom: 5 }}>
         <InputLabel
           sx={{
             color: '#B6B6B6',
@@ -55,7 +56,8 @@ function FlagEvents() {
             },
             '.MuiSvgIcon-root ': {
               fill: '#B6B6B6',
-            }
+            },
+            elevation: 12
           }}
           label="Region"
           onChange={handleChange}
@@ -76,7 +78,15 @@ function FlagEvents() {
       </FormControl>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         {rows.length > 0
-        && rows.map((row) => <FlagCard description={row['Description']} state={row['Origin Flag']} startDate={row['Start Date']} endDate={row['End Date']} />)
+        && rows.map((row, i) => {
+          return (
+            <Box width="100%">
+              <FlagCard description={row['Description']} state={row['Origin Flag']} startDate={row['Start Date']} endDate={row['End Date']} />
+              {(i < rows.length - 1) && <Divider sx={{ my: 3, backgroundColor: '#B6B6B6' }} />}
+            </Box>
+          )
+        }
+        )
         }
       </Box>
     </Box>
